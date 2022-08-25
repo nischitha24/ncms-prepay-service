76552018af0920b2c44e3e6e18ad752d4e51000e
NischithaSJ <nischitha.sj@numocity.com>
2022-08-25 16:32:33 +0530

    Changed read.js

--- 01_SourceCode/read.js
+++ 01_SourceCode/read.js
@@ -3,6 +3,7 @@ const yaml = require('js-yaml');
 const { spawn } = require('child_process')
 var releasetag = process.argv ;
 console.log(releasetag);
+console.log(typeof(releasetag))
 try {
     let fileContents = fs.readFileSync('./build_artifact.yml', 'utf8');
     let data = yaml.load(fileContents);
@@ -11,8 +12,9 @@ try {
     console.log(releasetag[2]);
     const name = data.docker_image.name;
     const tag = data.docker_image.tag;
-    const releaseTag = releasetag[2];
-    const backupProcess = spawn('docker',['build', '-t', `${name}:${tag+releaseTag}`,`.` , 
+    const tagArray = JSON.stringify(releasetag).split("/")
+    const version = tagArray[2];
+    const backupProcess = spawn('docker',['build', '-t', `${name}:${tag+version}`,`.` , 
     ]);
     backupProcess.stdout.on('data', (data) => {
         console.log('stdout:\n', data);
