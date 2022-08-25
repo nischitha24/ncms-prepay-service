585e8f806160bfe9ce4567222f6541bc586bd845
NischithaSJ <nischitha.sj@numocity.com>
2022-08-25 17:14:06 +0530

    tag

--- 01_SourceCode/read.js
+++ 01_SourceCode/read.js
@@ -3,7 +3,6 @@ const yaml = require('js-yaml');
 const { spawn } = require('child_process')
 var releasetag = process.argv ;
 console.log(releasetag);
-console.log(typeof(releasetag))
 try {
     let fileContents = fs.readFileSync('./build_artifact.yml', 'utf8');
     let data = yaml.load(fileContents);
@@ -12,9 +11,15 @@ try {
     console.log(releasetag[2]);
     const name = data.docker_image.name;
     const tag = data.docker_image.tag;
-    const tagArray = JSON.stringify(releasetag).split("/")
-    const version = tagArray[2];
-    const backupProcess = spawn('docker',['build', '-t', `${name}:${tag+version}`,`.` , 
+    const tagArray = JSON.stringify(releasetag[2]).split("/")
+    var version = "0"
+    if(tagArray.length == 1){
+      version = tagArray[0]
+    }
+    else{
+      version = tagArray[2]
+    }
+    const backupProcess = spawn('docker',['build', '-t', `${name}:${tag+version.substring(0,version.length - 1)}`,`.` , 
     ]);
     backupProcess.stdout.on('data', (data) => {
         console.log('stdout:\n', data);
